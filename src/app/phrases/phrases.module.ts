2decc66838f08af969ae2c8b0a3fc7809c7f47e9
olgasolda <olyasis@gmail.com>
2023-05-29 16:55:12 +0300

    simple feature routing

---
+++ src/app/phrases/phrases.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {PhrasesRoutingModule} from './phrases-routing.module';
+import {PhrasesListComponent} from "./phrases-list/phrases-list.component";
+import {PhraseDetailsComponent} from "./phrase-details/phrase-details.component";
+import { PhrasesHostComponent } from './phrases-host/phrases-host.component';
+
+@NgModule({
+  declarations: [
+    PhrasesListComponent,
+    PhraseDetailsComponent,
+    PhrasesHostComponent
+  ],
+  imports: [
+    CommonModule,
+    PhrasesRoutingModule
+  ]
+})
+export class PhrasesModule {
+}
