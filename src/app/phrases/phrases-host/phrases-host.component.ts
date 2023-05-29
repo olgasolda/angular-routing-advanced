c768565f7c3c4de337406bfd1cc689bb2924073b
olgasolda <olyasis@gmail.com>
2023-05-29 16:55:12 +0300

    relativeTo routing

---
+++ src/app/phrases/phrases-host/phrases-host.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phrases-host',
+  templateUrl: './phrases-host.component.html',
+  styleUrls: ['./phrases-host.component.scss']
+})
+export class PhrasesHostComponent {
+
+}
---
+++ src/app/phrases/phrases-routing.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PhrasesListComponent} from "./phrases-list/phrases-list.component";
+import {PhraseDetailsComponent} from "./phrase-details/phrase-details.component";
+
+const routes: Routes = [
+  {path: 'phrases', component: PhrasesListComponent},
+  {path: 'phrase/:id', component: PhraseDetailsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PhrasesRoutingModule {
+}
